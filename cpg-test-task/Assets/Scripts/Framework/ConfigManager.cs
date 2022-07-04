1c04affc7916f00daee285dcdb346ea2fee51360
Diegothic <diegothic24@gmail.com>
2022-07-04 10:14:22 +0100

    Config

---
+++ cpg-test-task/Assets/Scripts/Framework/ConfigManager.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using UnityEngine;
+
+namespace Framework
+{
+    public class ConfigManager : MonoBehaviour
+    {
+        [SerializeField]
+        private TextAsset configFile;
+
+        [Serializable]
+        public struct GridConfig
+        {
+            public int width;
+            public int height;
+        }
+
+        [Serializable]
+        public struct ConfigContent
+        {
+            public GridConfig grid;
+        }
+
+        private ConfigContent _config;
+
+        private void Awake()
+        {
+            LoadConfigFromFile();
+        }
+
+        private void LoadConfigFromFile()
+        {
+            _config = JsonUtility.FromJson<ConfigContent>(configFile.text);
+        }
+
+        public ConfigContent GetConfig()
+        {
+            return _config;
+        }
+    }
+}
